923af25ceaa192ebae0cbe4104da5acda445dca3
seveibar <seveibar@gmail.com>
2025-07-15 14:43:07 -0700

    initial cosmos page

--- lib/TscircuitIframe.tsx
+++ lib/TscircuitIframe.tsx
@@ -1,3 +1,84 @@
-export const TscircuitIframe = () => {};
+import { useEffect, useRef, useState } from "react"
 
-export default TscircuitIframe;
+export interface TscircuitIframeProps {
+  fsMap?: Record<string, string>
+  entrypoint?: string
+  code?: string
+  height?: string | number
+}
+
+export const TscircuitIframe = (runFrameProps: TscircuitIframeProps) => {
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  let additionalProps = {}
+
+  if (runFrameProps.code) {
+    additionalProps = {
+      fsMap: {
+        "index.tsx": runFrameProps.code,
+      },
+    }
+  }
+
+  if (runFrameProps.fsMap) {
+    additionalProps = {
+      fsMap: runFrameProps.fsMap,
+      entrypoint: runFrameProps.entrypoint,
+    }
+  }
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data?.runframe_type === "runframe_ready_to_receive") {
+        iframeRef.current?.contentWindow?.postMessage(
+          {
+            runframe_type: "runframe_props_changed",
+            runframe_props: { ...runFrameProps, ...additionalProps },
+          },
+          "*",
+        )
+        setIsLoading(false)
+      }
+    }
+
+    window.addEventListener("message", handleMessage)
+    return () => window.removeEventListener("message", handleMessage)
+  }, [])
+
+  return (
+    <div>
+      {isLoading && (
+        <div className="skeleton-container">
+          <div>
+            <div className="skeleton-header" />
+            <div className="skeleton-body" />
+          </div>
+        </div>
+      )}
+      <iframe
+        ref={iframeRef}
+        src="https://runframe.tscircuit.com/iframe.html"
+        title="tscircuit code runner and preview"
+        frameBorder="0"
+        scrolling="no"
+        style={{
+          overflow: "hidden",
+          width: "100%",
+          height: runFrameProps.height ?? 600,
+          border: "none",
+          padding: 0,
+          margin: 0,
+          boxSizing: "border-box",
+          display: isLoading ? "none" : "block",
+        }}
+        onLoad={() => {
+          // The iframe is loaded, but we'll only hide the skeleton
+          // when we receive the "runframe_ready_to_receive" message
+        }}
+      />
+    </div>
+  )
+}
+
+export default TscircuitIframe
--- pages/example01.page.tsx
+++ pages/example01.page.tsx
@@ -1,10 +1,59 @@
+import TscircuitIframe from "../lib/TscircuitIframe"
+
+const fsMap = {
+  "index.ts": `
+import { PushButton } from "@tsci/seveibar.push-button"
+import { SmdUsbC } from "@tsci/seveibar.smd-usb-c"
+
+export default () => {
+  return (
+    <board width="12mm" height="30mm">
+      <SmdUsbC
+        connections={{
+          GND1: "net.GND",
+          GND2: "net.GND",
+          VBUS1: "net.VBUS",
+          VBUS2: "net.VBUS",
+        }}
+        pcbY={-10}
+        schX={-4}
+      />
+      <led
+        name="LED"
+        supplierPartNumbers={{
+          jlcpcb: ["965799"],
+        }}
+        color="red"
+        footprint="0603"
+        pcbY={12}
+        schY={2}
+      />
+      <PushButton
+        name="SW1"
+        pcbY={0}
+        connections={{ pin2: ".R1 > .pos", pin3: "net.VBUS" }}
+        schY={-2}
+      />
+      <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />
+
+      <trace from=".R1 > .neg" to=".LED .pos" />
+      <trace from=".LED .neg" to="net.GND" />
+    </board>
+  )
+}
+  `,
+}
+
 export default () => (
   <div>
-	  <TscircuitIframe fsMap={{
-	    
-	  }} />
-<div>
-
-</div>
+    <TscircuitIframe fsMap={fsMap} />
+    <div>
+      Example usage:
+      <pre>
+        {`
+        <TscircuitIframe fsMap={{}} />
+        `}
+      </pre>
+    </div>
   </div>
-/>
+)
